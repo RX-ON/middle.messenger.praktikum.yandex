0f34885e76d755206b5c08ec343be06368c8226f
RX <fpreox@ya.ru>
2022-02-07 20:15:19 +0600

    Add scss lint

--- src/pages/chat-list/index.ts
+++ src/pages/chat-list/index.ts
@@ -1,11 +1,12 @@
 import checkValid from '../../common/scripts/utils/checkValid';
 import getFormData from '../../common/scripts/utils/getFormData';
 import renderPage from '../../common/scripts/utils/renderPage';
+import selectChat from '../../common/scripts/utils/selectChat';
 import MainLayout from '../../layout/main/main';
 import ChatListPage from './chat-list';
 
-
+const chat = selectChat();
 renderPage('#app', new MainLayout({
     content: new ChatListPage({}).render(),
-    handlers: {getFormData, checkValid}
+    handlers: {getFormData, checkValid, chat}
 }));
