f0f1ef92034c014b9f8d7c171aa35bb04404e93e
RX <fpreox@ya.ru>
2022-02-19 12:56:03 +0300

    update list

--- src/common/scripts/modules/Block.ts
+++ src/common/scripts/modules/Block.ts
@@ -5,7 +5,7 @@ export default class Block {
         INIT: 'init',
         FLOW_CDM: 'flow:component-did-mount',
         FLOW_CDU: 'flow:component-did-update',
-        FLOW_RENDER: 'flow:render'
+        FLOW_RENDER: 'flow:render',
     };
    
     _element: HTMLElement = document.createElement('template');
